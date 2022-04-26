276ba0a7a65da9ae799b3a70a10c778bd69aa1e3
Vitor Lima <vitorliima18@gmail.com>
2022-04-25 23:36:56 -0300

    Criaçao de telas: Login/TelaPrincipal/TelaSobre/TelaUsuário/TelaCliente ModuloConexao.

---
+++ src/br/com/infox/dal/ModuloConexao.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.infox.dal;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Vitor Lima
+ */
+public class ModuloConexao {
+    // método responsavel por estabelecer a conexão com o banco
+    
+    public static Connection conector(){
+        Connection conexao = null;
+        // a linha abaixo "chama" o driver
+        String driver = "com.mysql.cj.jdbc.Driver";
+        // Armazenando informações referente ao banco
+        String url = "jdbc:mysql://127.0.0.1:3306/dbinfox";
+        String user = "root";
+        String password = "vitorlima";
+        // Estabelecendo a conexao com o banco
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            // a linha abaixo serve de apoio para mostrar o erro
+            //System.out.println(e);
+            return null;
+        }
+        
+    }
+    
+    
+}
